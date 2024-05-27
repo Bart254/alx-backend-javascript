4591ef0ce49c177c22b06e17efecc73650d1385c
Bart254 <omondibartlett@gmail.com>
2024-05-28 02:00:08 +0300

    update 12-createeportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,11 +2,7 @@ export default function createReportObject(employeesList) {
   const myObject = {
     allEmployees: { ...employeesList },
     getNumberofDepartments(employeesList) {
-      let total = 0;
-      for (const deparment in employeesList) {
-        if (employeesList.hasOwnProperty(deparment)) total += 1;
-      }
-      return total;
+      return Object.keys(employeesList).length;
     },
   };
   return myObject;
