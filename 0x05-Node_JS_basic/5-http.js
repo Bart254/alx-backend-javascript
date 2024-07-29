159c8f5224ae3d28cc84ea45bd89cfce2e2f6109
Bart254 <omondibartlett@gmail.com>
2024-07-29 12:56:29 +0300

    add try/catch statement in function

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,37 +3,40 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    if (!path) reject(new Error('Cannot load the database'));
     fs.readFile(path, 'utf-8', (err, data) => {
       if (err) reject(new Error('Cannot load the database'));
 
       // remove first line and empty lines
-      let rows = data.split('\n');
-      delete rows[0];
-      rows = rows.filter((row) => row);
+      try {
+        let rows = data.split('\n');
+        delete rows[0];
+        rows = rows.filter((row) => row);
 
-      const numberStudents = rows.length;
-      const messA = `Number of students: ${numberStudents}`;
+        const numberStudents = rows.length;
+        const messA = `Number of students: ${numberStudents}`;
 
-      // find first names of each field
-      const cs = rows.filter((row) => row.split(',')[3] === 'CS').map(
-        (row) => row.split(',')[0],
-      );
-      const csNum = cs.length;
-      const csLst = cs.join(', ');
-      const messCS = `Number of students in CS: ${csNum}. List: ${csLst}`;
+        // find first names of each field
+        const cs = rows.filter((row) => row.split(',')[3] === 'CS').map(
+          (row) => row.split(',')[0],
+        );
+        const csNum = cs.length;
+        const csLst = cs.join(', ');
+        const messCS = `Number of students in CS: ${csNum}. List: ${csLst}`;
 
-      const swe = rows.filter((row) => row.split(',')[3] === 'SWE').map(
-        (row) => row.split(',')[0],
-      );
-      const sweNum = swe.length;
-      const sweLst = swe.join(', ');
-      const messSWE = `Number of students in SWE: ${sweNum}. List: ${sweLst}`;
+        const swe = rows.filter((row) => row.split(',')[3] === 'SWE').map(
+          (row) => row.split(',')[0],
+        );
+        const sweNum = swe.length;
+        const sweLst = swe.join(', ');
+        const messSWE = `Number of students in SWE: ${sweNum}. List: ${sweLst}`;
 
-      const fullMessage = `${messA}\n${messCS}\n${messSWE}`;
+        const fullMessage = `${messA}\n${messCS}\n${messSWE}`;
 
-      // call resolve with full message
-      resolve(fullMessage);
+        // call resolve with full message
+        resolve(fullMessage);
+      } catch (err) {
+        reject(new Error('Cannot load the database'));
+      }
     });
   });
 }
