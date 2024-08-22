082838c6dee72274c3cac50b228549c43fa4e0ae
Bart254 <omondibartlett@gmail.com>
2024-08-22 22:32:54 +0300

    add afterEach function to mocha test

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -10,17 +10,20 @@ describe('SendPaymentRequestToApi', () => {
 	beforeEach(() => {
 		spy = sinon.spy(console, 'log');
 	});
+
+	afterEach(() => {
+		spy.restore();
+	});
+
 	it('console must log the total is: 120', () => {
 		sendPaymentRequestToApi(100, 20);
 
 		expect(spy.calledWith('The total is: 120')).to.be.true;
-		spy.restore();
 	});
 
 	it('console must log the total is: 20', () => {
 		sendPaymentRequestToApi(10, 10);
 
 		expect(spy.calledWith('The total is: 20')).to.be.true;
-		spy.restore();
 	});
 });
