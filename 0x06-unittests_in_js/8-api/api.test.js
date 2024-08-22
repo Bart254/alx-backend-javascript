736c330d8b8fa38f7f7e301446f55ff57a124def
Bart254 <omondibartlett@gmail.com>
2024-08-23 01:53:35 +0300

    add done

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,10 +3,12 @@ const request = require('request')
 const { expect } = require('chai');
 
 describe('8-api', () => {
-	it('index page', () => {
+	it('index page', (done) => {
 		request('http://localhost:7865/', (err, response, body) => {
+			if (err) done(err);
 			expect(response.statusCode).to.equal(200);
 			expect(body).to.equal('Welcome to the payment system');
+			done();
 		});
 	});
 });
