ee0f939cf7013fcadf9a8727a6296c4316849f8c
Bart254 <113597942+Bart254@users.noreply.github.com>
2024-09-10 21:06:28 +0300

    Delete 0x05-Node_JS_basic directory

--- 0x05-Node_JS_basic/4-http.js
+++
@@ -1,8 +0,0 @@
-const { createServer } = require('http');
-
-const app = createServer((req, resp) => {
-  resp.end('Hello Holberton School!');
-});
-app.listen(1245, 'localhost');
-
-module.exports = app;
