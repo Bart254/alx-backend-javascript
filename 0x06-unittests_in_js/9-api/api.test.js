e47138813e0e80fbdc8c896371bc1eb2a543e133
Bart254 <omondibartlett@gmail.com>
2024-11-28 23:07:48 +0300

    modify api.test.js

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -3,23 +3,26 @@ const request = require('request')
 const { expect } = require('chai');
 
 describe('8-api', () => {
-	it('index page', () => {
+	it('index page', (done) => {
 		request('http://localhost:7865/', (err, response, body) => {
 			expect(response.statusCode).to.equal(200);
 			expect(body).to.equal('Welcome to the payment system');
 		});
+		done();
 	});
 
-	it('return code 200 with right message when cart is number', () => {
+	it('return code 200 with right message when cart is number', (done) => {
 		request('http://localhost:7865/cart/:12', (err, response, body) => {
 			expect(response.statusCode).to.equal(200);
 			expect(response.body).to.equal('Payment methods for cart 12');
 		});
+		done();
 	});
 
-	it('return 404 code when cart is non-numerical', () => {
+	it('return 404 code when cart is non-numerical', (done) => {
 		request('http://localhost:7865/cart/:wer', (err, response, body) => {
 			expect(response.statusCode).to.equal(404);
 		});
+		done();
 	});
 });
